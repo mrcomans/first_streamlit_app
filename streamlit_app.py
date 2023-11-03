ccdcca46984ac30ade027d60fad0fcfe575dee91
mrcomans <mark.teeken@gmail.com>
2023-11-03 12:07:37 +0100

    Commented out the last header

--- streamlit_app.py
+++ streamlit_app.py
@@ -44,5 +44,5 @@ my_data_rows = my_cur.fetchall()
 streamlit.header("The fruit load list contains:")
 streamlit.dataframe(my_data_rows)
 
-streamlit.header("What fruit would you like to add?")
+# streamlit.header("What fruit would you like to add?")
 add_my_fruit = streamlit.text_input('What fruit would you like to add?', 'kiwi')
