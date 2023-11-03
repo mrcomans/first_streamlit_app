9c171554811d57c9cf9bcbf0b3aa1c9166b6b269
mrcomans <mark.teeken@gmail.com>
2023-11-03 14:26:48 +0100

    adding skeleton code

--- streamlit_app.py
+++ streamlit_app.py
@@ -45,4 +45,8 @@ streamlit.header("The fruit load list contains:")
 streamlit.dataframe(my_data_rows)
 
 # streamlit.header("What fruit would you like to add?")
-add_my_fruit = streamlit.text_input('What fruit would you like to add?', 'jackfruit')
+add_my_fruit = streamlit.text_input('What fruit would you like to add?', 'jackfruit');
+streamlit.write('Thank you for adding ', add_my_fruit);
+my_cur.execute("insert into fruit_load_list values ('From streamlit')")
+
+
