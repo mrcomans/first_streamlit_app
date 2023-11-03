fe66df20dd86006769200a1a20d9300de07ed0bf
mrcomans <mark.teeken@gmail.com>
2023-11-03 15:01:33 +0100

    cleanup5

--- streamlit_app.py
+++ streamlit_app.py
@@ -4,7 +4,7 @@ import requests
 import snowflake.connector
 from urllib.error import URLError
 
-streamlit.title('My parents 9 new healthy diner')
+streamlit.title('My parents 10 new healthy diner')
 
 streamlit.header('Breakfast Favorites')
 streamlit.text('🥣 Omega 3 & Blueberry Oatmeal')
