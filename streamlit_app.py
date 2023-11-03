91d8e72bf4e59dedd862999fab38b072696ebf86
mrcomans <mark.teeken@gmail.com>
2023-11-03 15:04:18 +0100

    clean up 6

--- streamlit_app.py
+++ streamlit_app.py
@@ -25,14 +25,9 @@ fruits_to_show = my_fruit_list.loc[fruits_selected]
 streamlit.dataframe(fruits_to_show)
 
 def get_fruityvice_data(this_fruit_choice):
-        fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + this_fruit_choice)
-        # streamlit.text(fruityvice_response.json())
-
-        # take the json version and normalize it
-        fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
-    
+    fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + this_fruit_choice)
+    fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
     return fruityvice_normalized
-
 
 streamlit.header("Fruityvice Fruit Advice!")
 
