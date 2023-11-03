e001d2f30a80b1a6fd07da95ee4131e3aeb4b259
mrcomans <mark.teeken@gmail.com>
2023-11-03 15:42:20 +0100

    clean next

--- streamlit_app.py
+++ streamlit_app.py
@@ -49,14 +49,14 @@ my_cur = my_cnx.cursor()
 my_cur.execute("select * from pc_rivery_db.public.fruit_load_list")
 my_data_rows = my_cur.fetchall()
 # streamlit.text("Hello 1 from Snowflake:")
-streamlit.header("The fruit load list contains:")
+streamlit.header("View Our Fruit List - Add Your Favorites!")
 
 def get_fruit_load_list():
     with my_cnx.cursor() as my_cur:
         my_cur.execute("select * from fruit_load_list")
         return my_cur.fetchall()
 
-if streamlit.button('Get fruit load list'):
+if streamlit.button('Get Fruit List'):
     my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
     my_data_rows = get_fruit_load_list()
     streamlit.dataframe(my_data_rows)
